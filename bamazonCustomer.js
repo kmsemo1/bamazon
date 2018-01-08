dcb279521739e031f8d0899cc94a7cf9cfee91c8
kmsemo1 <32209919+kmsemo1@users.noreply.github.com>
2018-01-07 21:00:21 -0800

    inquirer added and initial test

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,23 +4,41 @@ var inquirer = require("inquirer");
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-  
+
     // Your username
     user: "root",
-  
+
     // Your password
     password: "",
     database: "top_songsDB"
-  });
-  
-  connection.connect(function(err) {
-    if (err) throw err;
-    displayTable();
-  });
+});
 
-  function displayTable() {
-      inquirer
-        .prompt({
-            
-        })
-  }
+connection.connect(function (err) {
+    if (err) throw err;
+    beginSearch();
+});
+
+function beginSearch() {
+    inquirer
+        .prompt([
+            {
+                name: "action",
+                type: "input",
+                message: "Please enter the ID of the product you would like to buy:"
+            },
+            {
+                name: "action",
+                type: "input",
+                message: "Please enter the number of units of the chosen item you would like to buy:"
+            }
+        ])
+        .then(function (answer) {
+            var query = "SELECT product_name, department_name, price, stock_quantit FROM bamazon WHERE ?";
+            connection.query(query, function (err, res) {
+                for (var i=0; i<res.length; i++) {
+                    console.log("ID: " + res[i].id);
+                }
+            }
+        )
+    })
+}
